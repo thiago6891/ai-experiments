2da1130ad2a590b66af8ed4521b08816a0c42d40
Thiago <thiago6891@gmail.com>
2014-03-21 11:18:32 -0300

    simple game framework implementation

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,24 @@
 define(function () {
 	return function (canvas) {
+		this.game_loop = function () {
+			this.update();
+			this.draw();
+		};
 		
+		this.update = function () {
+			for (var i = 0; i < this.objs.length; i++) {
+				this.objs[i].update();
+			}
+		};
+		
+		this.draw = function () {
+			for (var i = 0; i < this.objs.length; i++) {
+				this.objs[i].draw(this.ctx);
+			}
+		};
+		
+		this.objs = [];
+		this.ctx = canvas.getContext("2d");
+		this.loop = setInterval(this.game_loop, 17);
 	};
 });
