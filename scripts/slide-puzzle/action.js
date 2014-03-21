8945fccf707f7eb292bd1b7176440a908d41a5bb
Thiago <thiago6891@gmail.com>
2014-03-21 08:04:43 -0300

    action for the slide puzzle implemented.

--- scripts/slide-puzzle/action.js
+++ scripts/slide-puzzle/action.js
@@ -1,1 +1,8 @@
-﻿
+// An action is defined just by the square, located at the 
+// (x, y) position in the grid, that needs to be clicked.
+define(function () {
+	return function (x, y) {
+		this.x = x;
+		this.y = y;
+	}
+});
