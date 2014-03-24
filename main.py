2c410eb379fb81443f06c3438e11d83efd55986a
Thiago Cordeiro <thiago6891@gmail.com>
2014-03-24 18:08:32 -0300

    minor correction in code

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import jinja2
 import os
 import webapp2
 
-template_dir = os.path.join(os.path.dirname(__file__), "templates");
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(template_dir),
     autoescape = True)
